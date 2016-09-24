17aa4fa0434e5deb843507a09aa6d5642c8fdd3f
flaskeer <1185098948@qq.com>
2016-09-24 16:38:39 +0800

    fix bug

--- src/main/java/com/hao/spider/parse/JsoupParser.java
+++ src/main/java/com/hao/spider/parse/JsoupParser.java
@@ -1,6 +1,7 @@
 package com.hao.spider.parse;
 
 import com.hao.spider.http.HttpClient;
+import feign.Feign;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
@@ -12,7 +13,7 @@ import org.jsoup.nodes.Document;
 public class JsoupParser {
 
     public static Document parser(String absUrl,String prefixUrl) {
-        HtmlPage htmlPage = HttpClient.getInstance().action(HtmlPage.class, absUrl);
+        HtmlPage htmlPage = Feign.builder().target(HtmlPage.class, absUrl);
         String page = htmlPage.getHtmlPage(prefixUrl);
         return Jsoup.parse(page);
     }
